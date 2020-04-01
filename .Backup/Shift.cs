321cac51a3a884c8121fcdfcd75afa63d8d8c109
IAmLex <lexkrooswijk@live.nl>
2020-04-01 16:14:41 +0200

    Dikke update
    + Added ASP project
    + Added the right Forms project

---
+++ .Backup/Shift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DienstToevoegen
+{
+    class Shift
+    {
+        public string Event { get; set; }
+        
+        public int UserAmount { get; set; }
+        
+        public bool EventBool { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public User User { get; set; }
+
+        public Shift() { }
+
+        public Shift(DateTime start, DateTime end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public Shift(DateTime start, DateTime end, User user)
+        {
+            this.Start = start;
+            this.End = end;
+            this.User = user;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Start} - {this.End}: {this.User}";
+        }
+    }
+}
---
+++ BarBoekForms/BarBoekForms/Schedule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DienstToevoegen
+{
+    class Schedule
+    {
+        public List<Shift> Shifts { get; set; }
+
+        public Schedule()
+        {
+            this.Shifts = new List<Shift>();
+        }
+
+        public Schedule(Shift shift)
+        {
+            this.Shifts = new List<Shift>();
+            this.Shifts.Add(shift);
+        }
+
+        public Schedule(List<Shift> shifts)
+        {
+            this.Shifts = new List<Shift>();
+            this.Shifts.AddRange(shifts);
+        }
+
+        public void AddShift(Shift shift)
+        {
+            this.Shifts.Add(shift);
+        }
+
+        public void AddShifts(List<Shift> shifts)
+        {
+            this.Shifts.AddRange(shifts);
+        }
+
+        public void PlanShifts(List<User> users)
+        {
+            int count = 0;
+            foreach (Shift shift in this.Shifts)
+            {
+                foreach (User user in users)
+                {
+                    if (user.AddShift(shift))
+                    {
+                        // TODO: Add user to shift inside of schedule
+                        this.Shifts[count].User = user;
+
+                        break;
+                    }
+                }
+
+                count++;
+            }
+        }
+    }
+}
